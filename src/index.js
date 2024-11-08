fad8ac1fbbbbfc2a528eef44f75e513bc068eb3a
Dinindu Senanayake <dinindusen@gmail.com>
2024-11-08 14:55:17 +1300

    update react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import IOPerformanceDashboard from './IOPerformanceDashboard';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <IOPerformanceDashboard />
