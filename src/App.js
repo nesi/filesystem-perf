cf592ac03907a1e845cbce12cebef407ed4889bf
Dinindu Senanayake <dinindusen@gmail.com>
2024-11-08 16:05:00 +1300

    add config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IOPerformanceDashboard from './components/IOPerformanceDashboard';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <IOPerformanceDashboard />
+    </div>
+  );
+}
+
+export default App;
